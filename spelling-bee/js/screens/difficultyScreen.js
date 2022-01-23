efc5dc92ee2c89a70ec1609612508e825982a508
Judah_Daniels <34425995+judah-daniels@users.noreply.github.com>
2022-01-23 11:04:56 +0000

    Add Difficulty Screen

--- spelling-bee/js/screens/difficultyScreen.js
+++ spelling-bee/js/screens/difficultyScreen.js
@@ -1,34 +1,40 @@
-function MenuScreen () {
+function DifficultyScreen () {
     this.buttons = [];
 
     
-    this.buttons.push(new Button(width/2 - 100, height /2 , 100, 50, "Play",
+    this.buttons.push(new Button(width/2 - 100, height /2 , 100, 50, "Start Easy",
         function() {
             gameScreen.start(0);
+        }
+    ));
+
+    this.buttons.push(new Button(width/2 + 100, height /2 , 100, 50, "Start Hard",
+        function() {
+            gameScreen.start(1);
         }
     ));
     
 }
 
-MenuScreen.prototype.mouseClicked = function() {
+DifficultyScreen.prototype.mouseClicked = function() {
     for (var i = 0; i < this.buttons.length; i++) {
         this.buttons[i].mouseClicked();
     }
 };
 
-MenuScreen.prototype.update = function() {
+DifficultyScreen.prototype.update = function() {
     for (var i = 0; i < this.buttons.length; i++) {
         this.buttons[i].update();
     }
 };
 
-MenuScreen.prototype.draw = function() {
+DifficultyScreen.prototype.draw = function() {
     
     push();
     background(0,55,115);
     textSize(50);
     textAlign(CENTER);
-    text("SPELLING BEE TRAINER!", width/2, height / 4);
+    text("Select difficulty!", width/2, height / 4);
     pop();
 
     for (var i = 0; i < this.buttons.length; i++) {
