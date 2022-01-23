205e14ece3d4f0c1af55bd2cc3284b59a76bed83
Saksham Shah <saksham101@gmail.com>
2022-01-23 11:14:44 +0000

    Add how to play

--- spelling-bee/js/screens/difficultyScreen.js
+++ spelling-bee/js/screens/difficultyScreen.js
@@ -2,13 +2,13 @@ function DifficultyScreen () {
     this.buttons = [];
 
     
-    this.buttons.push(new Button(width/2 - 100, height /2 , 100, 50, "Start Easy",
+    this.buttons.push(new Button(width/2 - 100, height * 0.85 , 100, 50, "Easy",
         function() {
             gameScreen.start(0);
         }
     ));
 
-    this.buttons.push(new Button(width/2 + 100, height /2 , 100, 50, "Start Hard",
+    this.buttons.push(new Button(width/2 + 100, height * 0.85 , 100, 50, "Hard",
         function() {
             gameScreen.start(1);
         }
@@ -32,9 +32,19 @@ DifficultyScreen.prototype.draw = function() {
     
     push();
     background(0,55,115);
-    textSize(50);
     textAlign(CENTER);
-    text("Select difficulty!", width/2, height / 4);
+
+    textSize(50);
+    text("How to play", width/2, height/4);
+
+    textSize(20);
+    text("You will hear a word you have to spell.", width/2, height / 4 + 100)
+    text("You may use the buttons on the screen to gain more information about your word.", width/2, height / 4 + 130)
+    text("When you are ready to spell, click the record button.", width/2, height / 4 + 160)
+    text("Good luck!", width/2, height / 4 + 220)
+
+    textSize(30);
+    text("Select difficulty", width/2, height * 0.75);
     pop();
 
     for (var i = 0; i < this.buttons.length; i++) {
